dcdb618aa789a2bd38f8e3bf18a4083a7b8905f7
Daniel Cairns <danielcairns30@gmail.com>
2015-09-22 13:33:24 +1200

    creates registration form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,7 +6,30 @@
 	<div class="columns">
 		<h1>Trainer Registration</h1>
 
-		<form action="{{}}"></form>
+		<form action="/auth/register" method="post" novalidate>
+			{{csrf_field()}}
+
+			<div>
+				<label for="username">Username</label>
+				<input type="text" name="username" id="name" placeholder="Ash Ketchum" value="{{ old('name') }}">
+				{{ $errors->first('name')}}
+			</div>
+			<div>
+				<label for="email">Email</label>
+				<input type="email" id="email" name="email" placeholder="ash@ketchem.all" value="{{ old('email') }}">
+				{{ $errors->first('email')}}
+			</div>
+			<div>
+				<label for="password">Password</label>
+				<input type="password" id="password" name="password">
+				{{ $errors->first('password')}}
+			</div>
+			<div>
+				<label for="password_confirmation">Confirm Password</label>
+				<input type="password" id="password_confirmation" name="password_confirmation">
+			</div>
+			<input type="submit" value="Register" class="tiny button">
+		</form>
 	</div>
 </div>
 
