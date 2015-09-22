36f6dc9806ec176b69a0208b70976ea2d15ca959
Daniel Cairns <danielcairns30@gmail.com>
2015-09-22 13:18:13 +1200

    creates registration page

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,13 @@
+@extends('master')
+
+@section('content')
+
+<div class="row">
+	<div class="columns">
+		<h1>Trainer Registration</h1>
+
+		<form action="{{}}"></form>
+	</div>
+</div>
+
+@endsection
